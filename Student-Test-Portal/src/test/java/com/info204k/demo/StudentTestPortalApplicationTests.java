181b7ed9a4285637684a11abf08355315229fcb1
Rohit K <gundakallirohit@gmail.com>
2020-02-18 15:52:58 +0530

    Initail Release

---
+++ Student-Test-Portal/src/test/java/com/info204k/demo/StudentTestPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info204k.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTestPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
