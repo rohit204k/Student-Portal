fb77f7a7c732afbb4ff221634af180625607812d
Rohit K <gundakallirohit@gmail.com>
2020-02-20 10:25:15 +0530

    Data Creation Done

--- Student-Test-Portal/src/main/resources/data.sql
+++ Student-Test-Portal/src/main/resources/data.sql
@@ -8,3 +8,31 @@ INSERT INTO STUDENT values("1EL16DW007","Evans","Michael","Evans.Michael@yahoo.c
 INSERT INTO STUDENT values("1EL16DW008","Raveen","Lemont","Raveen.Lemont@yahoo.com","raveen");
 INSERT INTO STUDENT values("1EL16DW009","James","Mary","James.Mary@yahoo.com","james");
 INSERT INTO STUDENT values("1EL16DW010","Williams","Judy","Williams.Judy@yahoo.com","williams");
+
+INSERT INTO COURSES values("16DW01","English","24-02-2020");
+INSERT INTO COURSES values("16DW02","Hindi","26-02-2020");
+INSERT INTO COURSES values("16DW03","Kannada","28-02-2020");
+INSERT INTO COURSES values("16DW04","Social Science","25-02-2020");
+INSERT INTO COURSES values("16DW05","Maths","26-02-2020");
+INSERT INTO COURSES values("16DW06","Science","27-02-2020");
+
+insert into REGISTRATIONS values("1EL16DW001","16DW02","21-03-2017","75","YES");
+insert into REGISTRATIONS values("1EL16DW002","16DW06","17-09-2018","86","YES");
+insert into REGISTRATIONS values("1EL16DW003","16DW03","16-06-2019","48","YES");
+insert into REGISTRATIONS values("1EL16DW004","16DW01","23-05-2016","79","YES");
+insert into REGISTRATIONS values("1EL16DW005","16DW04","04-02-2018","81","YES");
+insert into REGISTRATIONS values("1EL16DW006","16DW02","09-07-2019","56","YES");
+insert into REGISTRATIONS values("1EL16DW007","16DW05","30-11-2017","63","YES");
+insert into REGISTRATIONS values("1EL16DW008","16DW06","25-10-2016","37","YES");
+insert into REGISTRATIONS values("1EL16DW009","16DW02","13-04-2016","78","YES");
+insert into REGISTRATIONS values("1EL16DW010","16DW01","26-12-2017","96","YES");
+insert into REGISTRATIONS values("1EL16DW001","16DW01","01-09-2016","50","YES");
+insert into REGISTRATIONS values("1EL16DW002","16DW05","30-05-2017","89","YES");
+insert into REGISTRATIONS values("1EL16DW003","16DW04","11-12-2018","15","YES");
+insert into REGISTRATIONS values("1EL16DW004","16DW06","13-10-2019","60","YES");
+insert into REGISTRATIONS values("1EL16DW005","16DW02","04-06-2016","35","YES");
+insert into REGISTRATIONS values("1EL16DW006","16DW02","25-07-2019","90","YES");
+insert into REGISTRATIONS values("1EL16DW007","16DW03","17-01-2018","23","YES");
+insert into REGISTRATIONS values("1EL16DW008","16DW03","29-02-2016","78","YES");
+insert into REGISTRATIONS values("1EL16DW009","16DW01","23-10-2017","98","YES");
+insert into REGISTRATIONS values("1EL16DW010","16DW06","12-04-2018","09","YES");
