7a4c798ba6482d1f4df9c7e23da718674a57625e
Rohit K <gundakallirohit@gmail.com>
2020-03-10 22:47:27 +0530

    A

--- Student-Test-Portal/src/main/java/com/info204k/demo/controller/CourseController.java
+++ Student-Test-Portal/src/main/java/com/info204k/demo/controller/CourseController.java
@@ -4,6 +4,7 @@ package com.info204k.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,13 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.info204k.demo.model.Courses;
 import com.info204k.demo.repo.CoursesRepo;
-
+@CrossOrigin(origins = { "http://localhost:3000"})
 @RestController
 public class CourseController 
 {
 	@Autowired
 	CoursesRepo repo;
-	
 	@GetMapping(path="/courses")
 	public List<Courses> getCourses()
 	{
